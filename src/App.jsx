4bd6630489a85a3dc3efcd1d92958ecddf98f982
Vincent C <vinch.cm@gmail.com>
2024-04-20 11:11:19 +0200

    quest done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-import "./App.css";
+import { useState } from "react";
+
+import MovieList from "./components/MovieList";
+import MovieContext from "./contexts/MovieContext";
 
 function App() {
+  const [movies, setMovies] = useState([
+    {
+      movie_id: 1,
+      title: "Harry Potter and the Sorcerers Stone",
+      release_year: 2001,
+    },
+    {
+      movie_id: 2,
+      title: "Harry Potter and the Chamber of Secrets",
+      release_year: 2002,
+    },
+    {
+      movie_id: 3,
+      title: "Harry Potter and the Prison of Azkaban",
+      release_year: 2004,
+    },
+  ]);
+
   return (
-    <>
-      <h1>Context API quest</h1>
-    </>
+    <MovieContext.Provider value={{ movies: movies, setMovies: setMovies }}>
+      <MovieList />
+    </MovieContext.Provider>
   );
 }
 
---
+++ src/components/MovieList.jsx
@@ -0,0 +1,41 @@
+import { useContext } from "react";
+import MovieContext from "../contexts/MovieContext";
+
+function MovieList() {
+  const { movies, setMovies } = useContext(MovieContext);
+
+  return (
+    <>
+      <h1>Movie List</h1>
+      {movies && (
+        <section>
+          <ul>
+            {movies.map((movie) => (
+              <li key={movie.id}>
+                {movie.title} ({movie.release_year})
+              </li>
+            ))}
+          </ul>
+        </section>
+      )}
+      {movies.length < 4 && (
+        <button
+          onClick={() =>
+            setMovies([
+              ...movies,
+              {
+                movie_id: 4,
+                title: "Harry Potter and the Goblet of Fire",
+                release_year: 2004,
+              },
+            ])
+          }
+        >
+          More
+        </button>
+      )}
+    </>
+  );
+}
+
+export default MovieList;
