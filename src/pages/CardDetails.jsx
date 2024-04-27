bb6bffac5aa94dd95f9d3d035b50540597503d38
Vincent C <vinch.cm@gmail.com>
2024-04-27 09:45:44 +0200

    step 4 completed with useLocation

--- src/pages/CardDetails.jsx
+++ src/pages/CardDetails.jsx
@@ -1,14 +1,22 @@
-import { useLoaderData, useNavigate } from "react-router-dom";
+import { useLoaderData, useNavigate, useLocation } from "react-router-dom";
 import Card from "../components/Card";
 
 function CardDetails() {
   const card = useLoaderData();
+  const location = useLocation();
   const navigate = useNavigate();
   // console.log(card);
   return (
     <>
       <main>
-        <Card card={card.data.data[0]} />;
+        <Card card={card.data.data[0]} />
+        {location.pathname !== "/" && (
+          <section>
+            <p>Description</p>
+            <p>Attaque</p>
+            <p>Défense</p>
+          </section>
+        )}
         <button type="button" onClick={() => navigate("/")}>
           Back to the list
         </button>
